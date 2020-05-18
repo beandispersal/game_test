8b9dceaac9e186d1d272b6d0718f5714f31c0c44
Bean <beandispersal@gmail.com>
2020-05-18 10:32:48 +0800

    page improvements

--- lib/Storage.dart
+++ lib/Storage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lwk/Bedroom.dart';
 import 'package:lwk/Garden.dart';
@@ -10,48 +11,203 @@ class Storage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('The COVID-19 Survival Guide')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(height: 30,),
+      body: Stack(
+        children: <Widget> [
+
+          Container(
+            width: 500,
+            height: 400,
+            child: Image(
+             fit:BoxFit.cover,
+             // height: double.infinity ,
+             // width: double.infinity,
+              image: AssetImage('assets/background.png')
+            ),
+          ),
+
+          Positioned(
+            child:
             Text('Storage',style: TextStyle(fontSize: 50,decoration: TextDecoration.underline),),
-            SizedBox(height: 100),
-            Text('Where do you wanna go?'),
-            SizedBox(height: 40),
-            RaisedButton(
-              onPressed: (){
-                Navigator.pushReplacement(context, ScaleRoute(page: Garden()));
-              },
-              child: Text('Get some seeds to plant'),
-            ),
-            RaisedButton(
-              onPressed: (){
-                Navigator.pushReplacement(context, ScaleRoute(page: Kitchen()));
-              },
-              child: Text('Get some food to cook'),
-            ),
-            RaisedButton(
+            top: 50,
+            left: 140,
+          ),
+          //  SizedBox(height: 30,),
+
+           // SizedBox(height: 100),
+          Text('Where do you wanna go?'),
+           // SizedBox(height: 40),
+
+
+          Positioned(
+            top: 120,
+            left: 100,
+            child: Center(
+          child: Column(
+            children: <Widget>[
+              RaisedButton(
               onPressed: (){
                 Navigator.pushReplacement(context, ScaleRoute(page: Telephone()));
               },
-              child: Text('Telephone'),
+              child: Container(
+                constraints: BoxConstraints.tightForFinite(
+                    width: 180
+                ),
+                child: Row(
+                children: <Widget>[
+                  Container(
+                    height: 30,
+                    width: 30,
+                    margin:EdgeInsets.fromLTRB(0, 0,10,0),
+                    child:
+                    Image(
+                        image:AssetImage('assets/telephone.png')
+                    ),
+                  ),
+                  Text('Telephone'),
+
+                    ]
+                    ),
+                  ),
+                ),
+            ],
+          ),
             ),
-            RaisedButton(
-              onPressed: (){
-                Navigator.pushReplacement(context, ScaleRoute(page: Outside()));
-              },
-              child: Text('Outside'),
+          ),
+
+
+
+          Positioned(
+            top: 170,
+            left: 100,
+            child: RaisedButton(
+            onPressed: (){
+              Navigator.pushReplacement(context, ScaleRoute(page: Kitchen()));
+            },
+          child:Container(
+            constraints: BoxConstraints.tightForFinite(
+                width: 180
             ),
-            RaisedButton(
-              onPressed: (){
-                Navigator.pushReplacement(context, ScaleRoute(page: Bedroom()));
-              },
-              child: Text('Bedroom'),
+            child:Row(
+              children: <Widget>[
+                Container(
+                  width:30,
+                  height:30,
+                  margin:EdgeInsets.fromLTRB(0, 0,10,0),
+                  child: Image(
+                    image: AssetImage('assets/cooking.png'),
+                  ),
+                ),
+                Text('Cook some food!'),
+
+              ],
             ),
-          ],
+          ),
+          ),
+          ),
+
+
+        Positioned(
+          top: 220,
+          left: 100,
+          child: RaisedButton(
+          onPressed: (){
+          Navigator.pushReplacement(context, ScaleRoute(page: Outside()));
+          },
+            child:
+            Container(
+          constraints: BoxConstraints.tightForFinite(
+              width: 180
+          ),
+          child: Row(
+
+            children: <Widget>[
+              Container(
+                width:30,
+                height:30,
+                margin:EdgeInsets.fromLTRB(0, 0,10,0),
+                child: Image(
+                  image: AssetImage('assets/park.png'),
+                ),
+              ),
+            Text('Outside'),
+            ],
+          ),
+            ),
+          ),
         ),
-      ),
-    );
+
+        RaisedButton(
+            onPressed: (){
+          Navigator.pushReplacement(context, ScaleRoute(page: Bedroom()));
+            },
+          child: Container(
+            constraints: BoxConstraints.tightForFinite(
+            width: 180
+            ),
+            child: Row(
+          children: <Widget>[
+            Container(
+              width:30,
+              height:30,
+              margin:EdgeInsets.fromLTRB(0, 0,10,0),
+              child: Image(
+                image: AssetImage('assets/bedroom.png'),
+
+              ),
+            ),
+
+           Text('Bedroom'),
+
+          ],
+            ),
+            ),
+          ),
+
+          RaisedButton(
+          onPressed: (){
+            Navigator.pushReplacement(context, ScaleRoute(page: Garden()));
+          },
+          child: Container(
+            constraints: BoxConstraints.tightForFinite(
+                width: 180
+            ),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width:30,
+                  height:30,
+                  margin:EdgeInsets.fromLTRB(0, 0,10,0),
+                  child: Image(
+                  image: AssetImage('assets/garden.png'),
+                  ),
+            ),
+                Text('Garden'),
+
+
+
+                  ],
+            ),
+          ),
+          ),
+          ],
+          ),
+        );
+
+
+
+
+
+
   }
 }
+
+
+//RaisedButton(
+//onPressed: (){
+//Navigator.pushReplacement(context, ScaleRoute(page: Kitchen()));
+//},
+//child:Stack(
+//children: <Widget> [
+//Text('Get some food to cook'),
+//],
+//),
